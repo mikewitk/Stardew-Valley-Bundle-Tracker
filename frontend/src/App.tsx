fba2189f0d93c27d66c55533669b53a5f3818db9
MIke Witk <mike.witk@gmail.com>
2021-12-27 16:31:41 -0500

    add ESLint and Prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>Stardew Valley Bundle Tracker</h1>
-  );
-}
-
+const App: React.FC = () => {
+  return <h1>Hello World</h1>;
+};
 export default App;
